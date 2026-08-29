b7fd110b6702aba174a321e25b20eeae5e97f989
alexdubskiycraft <alex.dubskiy.craft@gmail.com>
2026-08-29 14:34:23 +0300

    v1.1 Add model advert

--- advert/models.py
+++ advert/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
+from authentication.models import CustomUser
 
-# Create your models here.
+class Advert(models.Model):
+    class Priority(models.TextChoices):
+        INFO = "info", "Інформаційне"
+        REKLAMA = "reklama", "Рекламне"
+        SOCIAL = "social", "Соціальне"
+        ADMIN = "admin", "Адміністративне"
+
+    is_active = models.BooleanField(default=False)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=255)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="adverts")    
+    image = models.ImageField(
+        upload_to="adverts/",
+        verbose_name="Оголошення",
+        default=""
+    )
+    datetime_from = models.DateTimeField(default=timezone.now) 
+    datetime_to = models.DateTimeField(default=timezone.now) 
+    priority = models.IntegerField(choices=Priority.choices, default=Priority.INFO)
+    created_at = models.DateTimeField(auto_now_add=True)
+
