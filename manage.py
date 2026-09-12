598b620e880a1b9eb5d95d98c7938bcc730fbf94
Nordenchik <nordenchik228@gmail.com>
2026-09-12 13:59:00 +0300

    v2.0.2

--- manage.py
+++ manage.py
@@ -3,12 +3,10 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'group_portal.settings')
-    try:
-        from django.core.management import execute_from_command_line
+    try: from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -16,7 +14,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
