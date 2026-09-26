a76d13eda939f746ff37ca4eb4f89490bc4fa1a1
Nordenchik <nordenchik228@gmail.com>
2026-09-26 13:28:20 +0300

    v2.2.0

--- notebook/admin.py
+++ notebook/admin.py
@@ -3,6 +3,10 @@ from .models import StudentGrade
 
 @admin.register(StudentGrade)
 class StudentGradeAdmin(admin.ModelAdmin):
-    list_display = ('student', 'subject', 'grade', 'updated_at')
+    list_display = ('student', 'subject', 'grade', 'updated_at_formatted')
     list_filter = ('subject', 'grade')
     search_fields = ('student__username', 'subject', 'comment')
+
+    @admin.display(description='Дата')
+    def updated_at_formatted(self, obj):
+        return obj.updated_at.strftime('%d.%m.%Y %H:%M')
